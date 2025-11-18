4cc43ef6df2e1423134a7f03b5e611c9cd79799e
Naveenk0911 <naveenkumar092004@gmail.com>
2025-11-18 11:37:50 +0530

    ..

--- JS-Assignment-3/hosting_lab.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let score = 50;
-function announce() { console.log("Game started"); }
-announce();
-
-let status = "ready";
-const startGame = () => { console.log(status); };
-startGame();
--- JS-Assignment-3/strict_showdown.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function demo(a, b) {
-    let total = 10;
-    // delete total; // not allowed
-}
-demo(5, 10);
